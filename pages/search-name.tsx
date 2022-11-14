97168d8775812aa8304f3ee91daec0dfe065ca1f
Steven Matchett <stevenmatchett@Stevens-MacBook-Pro.local>
2022-11-13 23:29:40 -0500

    init stuff

--- pages/search-name.tsx
+++ pages/search-name.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import { SearchByName } from "../src/components/searchByName";
 
 const SearchName = () => {
-    return <SearchByName/>
+    const [driverData, setDriverData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('/driversEvents.json');
+            const data = await response.json();
+            setDriverData(data);
+        }
+        fetchData();
+        
+    }, [])
+    
+    return <SearchByName driverData={driverData} />
 }
 
 
--- src/components/searchByName.tsx
+++ src/components/searchByName.tsx
@@ -1,3 +1,42 @@
-export const SearchByName = () => {
-    return <></>
+import { useMemo, useState } from 'react';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+
+type Item = {
+    id: number;
+    name: string;
+}
+
+export const SearchByName = ({ driverData }: any) => {
+    const [selected, setSelected] = useState<any>(null);
+    const items = useMemo(() => {
+        if (!driverData) return [];
+        return Object.keys(driverData).map((driver: any, index: number) => {
+            return { id: index, name: driver }
+        })
+    }, [driverData.length]);
+
+    const handleOnSelect = (item: Item) => {
+        // the item selected
+        setSelected(driverData[item.name]);
+    }
+
+    const formatResult = (item: Item) => {
+        return (
+            <>
+                <span style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
+            </>
+        )
+    }
+    return (
+        <>
+            <ReactSearchAutocomplete
+                items={items}
+                onSelect={handleOnSelect}
+                autoFocus
+                formatResult={formatResult}
+            />
+            {selected && JSON.stringify(selected)}
+        </>
+    )
+
 }
